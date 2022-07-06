457c56247faaa748b9ffd1ee66e750577daa70ab
al-amin <arifurr231@gmail.com>
2022-07-06 14:34:37 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Share/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Contact from "./Contact/Contact";
+import Login from "./Login/Login";
+import Reviews from "./Pages/Reviews/Reviews";
+import Appointment from "./Pages/Appointment/Appointment";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path='/' element={} />
+        <Route path='/' element={} />
+        <Route path='/' element={} /> */}
+      </Routes>
     </div>
   );
 }
